2ff771c618b1b9b3ee454e980c09b472bedd54d7
Tapoban Ray <107751519+tapoban123@users.noreply.github.com>
2023-06-21 22:11:34 +0530

    Added the project files

---
+++ app.py
@@ -0,0 +1,40 @@
+# My First Flask web application 
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+    {'id': 1,
+     'title': 'Data Analyst',
+     'location' : 'Bangaluru, India',
+     'salary': '₹ 10,00,000'
+     },
+    {'id': 2,
+     'title': 'Data Scientist',
+     'location' : 'Delhi, India',
+     'salary': '₹ 15,00,000'
+     },
+    {'id': 3,
+     'title': 'FrontEnd Engineer',
+     'location' : 'Remote',
+    #  'salary': '₹ 12,00,000'
+     },
+    {'id': 4,
+     'title': 'Backend Engineer',
+     'location' : 'San Francisco, USA',
+     'salary': '$ 120,000'
+     },
+]
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home2.html', jobs=JOBS, company_name='Jovian')
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
+if '__name__' == '__main__':
+    app.run(host='0.0.0.0', debug=True)
