f41e54ef271d43732d20f2eb17e70c485a8e30df
Tapoban Ray <107751519+tapoban123@users.noreply.github.com>
2023-07-26 00:16:29 +0530

    Added jobpage.html and shared code wihtout copying them

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
 # My First Flask web application 
 
 from flask import Flask, render_template, jsonify
-from database import jobs_import_from_db
-
+from database import jobs_import_from_db, job_import_from_db
 
 
 app = Flask(__name__)
-
-
-
 
 @app.route('/')
 def hello_world():
     jobs_data = jobs_import_from_db()
     return render_template('home2.html', jobs=jobs_data, company_name='Jovian')
 
+@app.route('/api/jobs')
+def list_jobs():
+    jobs = jobs_import_from_db()
+    return jsonify(jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = job_import_from_db(id)
+    if job == None:
+        # return render_template('job_not_found.html')
+        return "NOT FOUND, 404"
+    else:
+        return render_template('jobpage.html', job=job)
+    # return jsonify(job)
+    
 
 
 if '__name__' == '__main__':
--- database.py
+++ database.py
@@ -28,6 +28,23 @@ def jobs_import_from_db():
         job_infos.append(dict(ID=rec[0], Title=rec[1], Location=rec[2], Salary=rec[3], Currency=rec[4], Responsibilities=rec[5], Requirements=rec[6]))
     return job_infos
 
+
+def job_import_from_db(job_id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f'SELECT * FROM JOBS WHERE ID = {job_id}'))
+        rows = result.all()
+    
+    if len(rows) == 0:
+        return None
+
+    else:
+        job = rows[0]
+        job_values = dict(ID=job[0], Title=job[1], Location=job[2], Salary=job[3], Currency=job[4], Responsibilities=job[5], Requirements=job[6])
+        return job_values  
+
+
+
+
 # with engine.connect() as conn:
 #     result = conn.execute(text('SELECT * FROM JOBS;'))
 #     data = result.all()
